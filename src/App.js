c238474e2f616f56f9c5539f456a9e0a705134b9
Jorge Luis Flores Assad <102478345+seinenten@users.noreply.github.com>
2023-10-12 13:20:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CryptoJS from "crypto-js";
 function App() {
+
+  const cifrar=(texto)=>{
+    var textoCifrado=CryptoJS.AES.encrypt(texto,'@borjascript').toString();
+    return textoCifrado;
+  }
+
+  const descifrar=(texto)=>{
+    var bytes=CryptoJS.AES.decrypt(texto,'@borjascript');
+    var textoDescifrado=bytes.toString(CryptoJS.enc.Utf8);
+    return textoDescifrado;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <p>Texto Cifrado: {cifrar("hola")}</p>
+    <p>Texto Descifrado: {descifrar("U2FsdGVkX18eo2DlodR6K00VB2wkEujnWECHmEx9Bns=")}</p>
     </div>
+    
   );
 }
-
 export default App;
